142008dc16b385f1658876b00a52cee24b9ce7c3
Marek Bartos <bartos.developer152@gmail.com>
2018-10-03 14:49:05 +0200

    Simplify ApiMiddleware

--- src/ApiMiddleware.php
+++ src/ApiMiddleware.php
@@ -3,12 +3,11 @@
 namespace Apitte\Middlewares;
 
 use Apitte\Core\Dispatcher\IDispatcher;
-use Apitte\Core\Exception\Logical\InvalidStateException;
+use Contributte\Middlewares\IMiddleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Throwable;
 
-class ApiMiddleware
+class ApiMiddleware implements IMiddleware
 {
 
 	/** @var IDispatcher */
@@ -22,30 +21,12 @@ class ApiMiddleware
 	public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next): ResponseInterface
 	{
 		// Pass this API request/response objects to API dispatcher
-		$response = $this->dispatch($request, $response);
+		$response = $this->dispatcher->dispatch($request, $response);
 
 		// Pass response to next middleware
 		$response = $next($request, $response);
 
 		return $response;
-	}
-
-	protected function dispatch(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
-	{
-		try {
-			// Pass to dispatcher, find handler, process some logic and return response.
-			$response = $this->dispatcher->dispatch($request, $response);
-
-			// Validate returned api response
-			if (!($response instanceof ResponseInterface)) {
-				throw new InvalidStateException(sprintf('Returned response must be type of %s', ResponseInterface::class));
-			}
-
-			return $response;
-		} catch (Throwable $e) {
-			// Just throw this out
-			throw $e;
-		}
 	}
 
 }
