06b75ab906795f5f872c7aa85414c0703d6b14e2
Marek Bartos <bartos.developer152@gmail.com>
2019-03-31 01:32:47 +0100

    Enforce ApiRequest and ApiResponse

--- src/ApiMiddleware.php
+++ src/ApiMiddleware.php
@@ -3,6 +3,8 @@
 namespace Apitte\Middlewares;
 
 use Apitte\Core\Dispatcher\IDispatcher;
+use Apitte\Core\Http\ApiRequest;
+use Apitte\Core\Http\ApiResponse;
 use Contributte\Middlewares\IMiddleware;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -20,6 +22,14 @@ class ApiMiddleware implements IMiddleware
 
 	public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next): ResponseInterface
 	{
+		if (!$request instanceof ApiRequest) {
+			$request = new ApiRequest($request);
+		}
+
+		if (!$response instanceof ApiResponse) {
+			$response = new ApiResponse($response);
+		}
+
 		// Pass this API request/response objects to API dispatcher
 		$response = $this->dispatcher->dispatch($request, $response);
 
--- tests/cases/ApiMiddleware.phpt
+++ tests/cases/ApiMiddleware.phpt
@@ -5,6 +5,8 @@
  */
 
 use Apitte\Core\Dispatcher\IDispatcher;
+use Apitte\Core\Http\ApiRequest;
+use Apitte\Core\Http\ApiResponse;
 use Apitte\Middlewares\ApiMiddleware;
 use Contributte\Middlewares\Utils\Lambda;
 use Contributte\Psr7\Psr7ResponseFactory;
@@ -15,8 +17,8 @@ use Tester\Assert;
 require_once __DIR__ . '/../bootstrap.php';
 
 Toolkit::test(function (): void {
-	$request = Psr7ServerRequestFactory::fromSuperGlobal();
-	$response = Psr7ResponseFactory::fromGlobal();
+	$request = new ApiRequest(Psr7ServerRequestFactory::fromSuperGlobal());
+	$response = new ApiResponse(Psr7ResponseFactory::fromGlobal());
 
 	$dispatcher = Mockery::mock(IDispatcher::class);
 	$dispatcher->shouldReceive('dispatch')
